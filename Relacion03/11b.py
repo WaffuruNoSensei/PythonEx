f71f7ff96245814ceaa9a26c212197250480fb59
WaffuruNoSensei <Waffurunosensei@gmail.com>
2021-02-07 18:03:56 +0100

    py

---
+++ Relacion03/11b.py
@@ -0,0 +1,25 @@
+"""
+Ejercicio 11bc
+Escribir una función que reciba dos números como parámetros, y devuelva
+cuántos múltiplos del primero hay, que sean menores que el segundo.
+b) Implementarla utilizando un ciclo while, que multiplique el primer número
+hasta que sea mayor que el segundo.
+"""
+
+
+def numMult(m, n):
+    i = 1
+    mul = m
+    while mul <= n:
+        i += 1
+        mul *= i
+    return i - 1
+
+
+print(numMult(12, 360))
+
+"""
+c) Comparar ambas implementaciones: ¿Cuál es más clara? ¿Cuál realiza menos
+operaciones?
+La primera es más clara pero la segunda es manifiestamente más rápida.
+"""
---
+++ Relacion04/04d.py
@@ -0,0 +1,15 @@
+"""
+d) Escribir una función que reciba un vector y devuelva su norma.
+>>> magnitude((3,4))
+5.0
+>>> magnitude((3,4,5))
+7.0710678118654755
+"""
+
+
+def magnitude(x):
+    sum = 0
+    for i in range(len(x)):
+        sum += x[i] * x[i]
+    return sum ** (1/2)
+
